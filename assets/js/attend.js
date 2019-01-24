e6e170bc18f37c963bfbb5b8bc1f5ca1fca099a7
kamwanasamwel <kamwanasamwel@gmail.com>
2019-01-24 08:58:33 +0300

    added download and print option

--- assets/js/attend.js
+++ assets/js/attend.js
@@ -18,10 +18,11 @@ var atdata = document.getElementById('attdlist');
 var nos = 0;
 
 document.getElementById('clsattd').addEventListener('submit', ClassAttend);
-function ClassAttend(e){
+
+function ClassAttend(e) {
     e.preventDefault();
     // add titles
-    ttitles.innerHTML = "<h2>Dedan Kimathi University of Science and Technology</h2> <h4>Course: "+document.getElementById('cocourse').value+" Year: "+document.getElementById('coyos').value+" Semester: "+document.getElementById('cosem').value+"</h4><h4>Attendance Sheet</h4>"
+    ttitles.innerHTML = "<h2>Dedan Kimathi University of Science and Technology</h2> <h4>Course: " + document.getElementById('cocourse').value + " Year: " + document.getElementById('coyos').value + " Semester: " + document.getElementById('cosem').value + "</h4><h4>Attendance Sheet for " + document.getElementById('datepicker').value + "</h4>"
     // query data
     // firestore.collection("StudentScanClass").where("unitname", "==", "Project Management ")
     // .get()
@@ -36,17 +37,36 @@ function ClassAttend(e){
     // });
     // old query
     firestore.collection("StudentScanClass").where("year", "==", document.getElementById('coyear').value).where("semester", "==", document.getElementById('cosem').value).where("course", "==", document.getElementById('cocourse').value).where("yearofstudy", "==", document.getElementById('coyos').value).where("querydate", "==", document.getElementById('datepicker').value).where("unitname", "==", document.getElementById('counit').value)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            nos ++;
-            // doc.data() is never undefined for query doc snapshots
-            atdata.innerHTML += "<tr><th scope='row'>"+nos+"</th> <td> "+doc.data().regno+" </td> <td> "+doc.data().studname+" </td></tr>"
-            console.log(doc.id, " => ", doc.data());
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                nos++;
+                // doc.data() is never undefined for query doc snapshots
+                atdata.innerHTML += "<tr><th scope='row'>" + nos + "</th> <td> " + doc.data().regno + " </td> <td> " + doc.data().studname + " </td></tr>"
+                console.log(doc.id, " => ", doc.data());
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
         });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
 
+}
+//printing of attendance sheet
+document.getElementById("PrintButton").addEventListener("click", PrintResults);
+
+function PrintResults(ptable) {
+    console.log("I was clicked and I hope to output an attendance sheet");
+    var content = document.getElementById("ptable").innerHTML;
+    var mywindow = window.open("", "Print", "height=600,width=800");
+
+    // mywindow.document.write('<html><head><title>Print</title>');
+    // mywindow.document.write('</head><body >');
+    mywindow.document.write(content);
+    // mywindow.document.write('</body></html>');
+
+    mywindow.document.close();
+    mywindow.focus();
+    mywindow.print();
+    mywindow.close();
+    return true;
 }
--- assets/js/lists.js
+++ assets/js/lists.js
@@ -1,18 +1,16 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyATsOBvPIaOwQUfV6WDy4yzm2dRUm-kXdE",
-    authDomain: "showcase-3debf.firebaseapp.com",
-    databaseURL: "https://showcase-3debf.firebaseio.com",
-    projectId: "showcase-3debf",
-    storageBucket: "showcase-3debf.appspot.com",
-    messagingSenderId: "3641612382"
+  apiKey: "AIzaSyATsOBvPIaOwQUfV6WDy4yzm2dRUm-kXdE",
+  authDomain: "showcase-3debf.firebaseapp.com",
+  databaseURL: "https://showcase-3debf.firebaseio.com",
+  projectId: "showcase-3debf",
+  storageBucket: "showcase-3debf.appspot.com",
+  messagingSenderId: "3641612382"
 };
 firebase.initializeApp(config);
 
 var firestore = firebase.firestore();
 var user = firebase.auth().currentUser;
-
-
 
 // listing of courses array on drop down
 
@@ -22,8 +20,8 @@ var user = firebase.auth().currentUser;
 // crsRef.get().then(function(doc) {
 //     if (doc.exists) {
 //       var i;
-//       for (i = 1; i < 25; i++) { 
-//         dcourse.innerHTML += "<option>"+ doc.data()[i] +"</option>"; 
+//       for (i = 1; i < 25; i++) {
+//         dcourse.innerHTML += "<option>"+ doc.data()[i] +"</option>";
 //       }
 //       // dcourse.innerHTML += "<option>"+ doc.data() +"</option>";
 //         console.log("Document data:", doc.data());
@@ -35,46 +33,84 @@ var user = firebase.auth().currentUser;
 // }).catch(function(error) {
 //     console.log("Error getting document:", error);
 // });
-var ttitle = document.getElementById('title');
-var todata = document.getElementById('tdata');
+var ttitle = document.getElementById("title");
+var todata = document.getElementById("tdata");
 var nos = 0;
 
-document.getElementById('clslist').addEventListener('submit', ClassList);
-function ClassList(e){
-    e.preventDefault();
-    // add titles
-    ttitle.innerHTML = "<h2>Dedan Kimathi University of Science and Technology</h2> <h4>Course: "+document.getElementById('ccourse').value+" Year: "+document.getElementById('cyos').value+" Semester: "+document.getElementById('csem').value+"</h4><h4>Class List</h4>"
-    firestore.collection("StudentDetails").where("currentyear", "==", document.getElementById('cyear').value).where("currentsemester", "==", document.getElementById('csem').value).where("course", "==", document.getElementById('ccourse').value).where("yearofstudy", "==", document.getElementById('cyos').value)
+document.getElementById("clslist").addEventListener("submit", ClassList);
+
+function ClassList(e) {
+  e.preventDefault();
+  // add titles
+  ttitle.innerHTML =
+    "<h2 style='margin=0px;'>Dedan Kimathi University of Science and Technology</h2> <h4 style='margin=0px;'>Course: " +
+    document.getElementById("ccourse").value +
+    " Year: " +
+    document.getElementById("cyos").value +
+    " Semester: " +
+    document.getElementById("csem").value +
+    "</h4><h4 margin-top:-5px; margin-bottom: 0px;>Class List</h4>";
+  firestore
+    .collection("StudentDetails")
+    .where("currentyear", "==", document.getElementById("cyear").value)
+    .where("currentsemester", "==", document.getElementById("csem").value)
+    .where("course", "==", document.getElementById("ccourse").value)
+    .where("yearofstudy", "==", document.getElementById("cyos").value)
     .get()
     .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            nos ++;
-            // doc.data() is never undefined for query doc snapshots
-            todata.innerHTML += "<tr></tr><th scope='row'>"+nos+"</th> <td> "+doc.data().regnumber+" </td> <td> "+doc.data().firstname+" </td> <td> "+doc.data().lastname+" </td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>"
-            console.log(doc.id, " => ", doc.data());
-        });
+      querySnapshot.forEach(function(doc) {
+        nos++;
+        var space = "    ";
+        // doc.data() is never undefined for query doc snapshots
+        todata.innerHTML +=
+          "<tr></tr><th scope='row' width='20'>" +
+          nos +
+          "</th> <td width='150'> " +
+          doc.data().regnumber +
+          " </td> <td width='120'> " +
+          doc.data().firstname +
+          " " +
+          doc.data().lastname +
+          " </td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td><td width='40'></td></tr>";
+        console.log(doc.id, " => ", doc.data());
+      });
     })
     .catch(function(error) {
-        console.log("Error getting documents: ", error);
+      console.log("Error getting documents: ", error);
     });
-
 }
 
-// printing of attendance list
+// downloading of attendance list
+document.getElementById("DownloadButton").addEventListener("click", PrintPDF);
+document.getElementById("PrintButton").addEventListener("click", PrintResults);
+function PrintPDF(e, ptable) {
+  e.preventDefault();
+  console.log("Am clicked.. Hoping I will generate PDF");
+  var doc = new jsPDF();
+  var specialElementHandlers = {
+    "#editor": function(element, renderer) {
+      return true;
+    }
+  };
+  doc.fromHTML($("#ptable").html(), 15, 15, {
+    width: 230,
+    elementHandlers: specialElementHandlers
+  });
+  doc.save();
+}
 
-document.getElementById('DownloadButton').addEventListener('click', PrintPDF);
-function PrintPDF(e){
-    e.preventDefault();
-    console.log("Am clicked.. Hoping I will generate PDF");
-    var doc = new jsPDF();
-    var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-            return true;
-        }
-    };
-    doc.fromHTML($('#ptable').html(), 15, 15, {
-        'width': 230,
-        'elementHandlers': specialElementHandlers
-    });
-    doc.save();
+function PrintResults(ptable) {
+  var content = document.getElementById("ptable").innerHTML;
+  var mywindow = window.open("", "Print", "height=600,width=800");
+
+  // mywindow.document.write('<html><head><title>Print</title>');
+  // mywindow.document.write('</head><body >');
+  mywindow.document.write(content);
+  // mywindow.document.write('</body></html>');
+
+  mywindow.document.close();
+  mywindow.focus();
+  mywindow.print();
+  mywindow.close();
+  return true;
 }
